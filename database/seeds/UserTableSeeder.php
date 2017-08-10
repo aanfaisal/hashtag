eed3bea2a065ee23fa42668c05df922d9082ee11
anisku11 <aan.faisalal@gmail.com>
2017-08-11 00:13:42 +0700

    add seeder user

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            'name' => 'Admin',
+            'email' => 'hashtagapp@gmail.com',
+            'password' => bcrypt('hashtag'),
+            ]);
+    }
+}
